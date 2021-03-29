83be72a5332e5025e710fe473988e2dfe794a2ac
itayzaga <itayzaga93@gmail.com>
2021-03-29 03:27:13 +0300

    first

---
+++ text.py
@@ -0,0 +1,18 @@
+import math
+def revword (word):
+	newword = ""
+	for letter in word:
+		newword = letter + newword
+	return newword.lower()
+
+def countword ():
+	file = open('C:/Users/Moti/Desktop/text.txt')
+	word = file.readline().strip().lower()
+	count = 0
+	for line in file:
+		for switchword in line.split():
+			if revword(switchword) == word:
+				count = count + 1
+	return count + 1
+
+print(countword())
